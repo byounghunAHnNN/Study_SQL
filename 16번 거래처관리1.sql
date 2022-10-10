7c3b9fe2d72648eb97f44b9faa040f6b694bdffd
dksqudgns <97275688+byounghunAHnNN@users.noreply.github.com>
2022-10-10 22:40:15 +0900

    sql 쿼리 연습

---
+++ 16번 거래처관리1.sql
@@ -0,0 +1,10 @@
+select co.cop_date
+      ,co.cop_nm
+      ,co.cop_addr
+      ,co.cop_memo
+      ,co.cop_ee
+      ,co.cop_tel
+      ,co.cop_phn
+      ,co.cop_email
+
+ from tb_company co 
---
+++ 19번 sql.sql
@@ -0,0 +1,4 @@
+select dp.dept_cd
+      ,dp.dept_name
+
+from tb_dept dp 
