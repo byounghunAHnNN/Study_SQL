1567e91e56d3d787eb1e6472be382c3155436e91
dksqudgns <97275688+byounghunAHnNN@users.noreply.github.com>
2022-10-17 18:35:50 +0900

    영업계획 쿼리 연습 + 교육관리 시스템 31번

---
+++ 10.17 21번 추가 쿼리..sql
@@ -0,0 +1,5 @@
+select dv.dva_tot
+      ,dv.dva_tot - dv.dva_rest
+      ,dv.dva_rest
+
+from tb_dtlvacation dv
---
+++ 31번 교육관리.sql
@@ -0,0 +1,7 @@
+select lc.le_title
+      ,ln.in_date
+      ,lc.le_nm
+      
+
+from tb_lecdata lc
+      inner join tb_lec_notice ln 
